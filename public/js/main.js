70877797c3b80cea993e0c8fac66a97a165a0cec
Ярослав <Yaroslav@MacBook-Air-Cybermart.local>
2025-08-28 07:27:44 +0500

    Update catalog.html and add PWA manifest

--- public/js/main.js
+++ public/js/main.js
@@ -12,24 +12,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
 (function () {
   var a = document.querySelectorAll('.nav-list a');
-  var currentPath = window.location.pathname;
-  var currentHref = window.location.href;
-  
-  // Нормализуем пути для сравнения
-  var normalizedPath = currentPath.replace(/\/$/, '') || '/'; // Убираем trailing slash, кроме корня
-  var isHomePage = normalizedPath === '/' || normalizedPath === '/index.html' || normalizedPath === '/index.htm' || currentHref === window.location.origin + '/';
-  
   for (var i = a.length; i--;) {
-    var linkHref = a[i].getAttribute('href');
-    var normalizedLink = linkHref.replace(/\/$/, '') || '/';
-    
-    // Для главной страницы проверяем несколько вариантов
-    if (isHomePage && (normalizedLink === '/' || linkHref === 'index.html' || linkHref === './index.html')) {
-      a[i].className += ' active';
-    }
-    // Для остальных страниц обычное сравнение
-    else if (normalizedLink === normalizedPath || a[i].href === currentHref) {
+    if (a[i].href === window.location.pathname || a[i].href === window.location.href) {
       a[i].className += ' active';
     }
   }
-})();;
+})();
