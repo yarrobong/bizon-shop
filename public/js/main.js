8f484dc56bd871484b75ccdaab9c22cec6bb5c57
Ярослав <Yaroslav@MacBook-Air-Cybermart.local>
2025-09-01 14:59:22 +0500

    category cool, icons

--- public/js/main.js
+++ public/js/main.js
@@ -10,4 +10,18 @@ document.addEventListener('DOMContentLoaded', async () => {
   
   // Загружаем и отображаем товары
   await renderProducts();
+  
+  
 });
+
+const path = window.location.pathname;
+let pageKey = 'index'; // по умолчанию — главная
+
+if (path.includes('catalog.html')) pageKey = 'catalog';
+else if (path.includes('about.html')) pageKey = 'about';
+else if (path.includes('contact.html')) pageKey = 'contact';
+
+const activeLink = document.querySelector(`.main-nav a[data-page="${pageKey}"]`);
+if (activeLink) {
+  activeLink.classList.add('active');
+}
